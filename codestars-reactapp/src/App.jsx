03629e9fe59a57dce2c1eccf225c2d8c6fa7e811
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-05-23 15:05:40 +0200

    refactored activeBtnIndex

--- codestars-reactapp/src/App.jsx
+++ codestars-reactapp/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/header/Header.jsx'
-import Navigation from './components/nav/Navigation.jsx'
+import Navigation from './components/navigation/Navigation.jsx'
 import './App.css'
 
 function App() {
