8f3ef227f87069809513f0c8b06b863f9a453c0e
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-05-23 15:26:07 +0200

    rv: minor fixes to Feed.jsx

--- codestars-reactapp/src/App.jsx
+++ codestars-reactapp/src/App.jsx
@@ -2,15 +2,14 @@ import Header from './components/header/Header.jsx'
 import Navigation from './components/navigation/Navigation.jsx'
 import './App.css'
 import Postcard from './components/postcard/Postcard.jsx'
+import Feed from './pages/Feed.jsx'
 
 function App() {
 
   return (
     <>
       {/* Here you are supposed to use components */}
-        < Postcard /> 
-
-        <Navigation/>
+        <Feed />
     </>
   )
 }
