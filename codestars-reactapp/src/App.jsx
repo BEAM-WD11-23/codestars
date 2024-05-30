be8fd4c6f93297a85debf553d811ea60d6c212be
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-05-30 15:25:30 +0200

    implemented notifications page

--- codestars-reactapp/src/App.jsx
+++ codestars-reactapp/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-import Feed from './pages/Feed.jsx'
+// import Feed from './pages/Feed.jsx'
+import Navbar from './components/navigation/Navigation'; // Import your Navbar component
+import Notifications from './pages/notifications/Notifications';
+import Header from './components/header/Header' // Import the Notifications component
 
 function App() {
 
   return (
     <>
       {/* Here you are supposed to use components */}
-        <Feed />
+        {/* <Feed /> */}
+      <Notifications />
+     
     </>
   )
 }
