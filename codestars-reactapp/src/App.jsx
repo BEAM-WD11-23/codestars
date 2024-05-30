f04ba1fc894e2b5c1acecaaead3aacaf31284aa4
Joel.M <joel.moran@students.beaminstitute.org>
2024-05-30 15:22:19 +0200

    Added the RegistrationForm page folder with its corresponding jsx, css and styled.js files; fixed some issues using framer-motion for hover and isChecked on the RegistrationForm.jsx file

--- codestars-reactapp/src/App.jsx
+++ codestars-reactapp/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Feed from './pages/Feed.jsx'
+import RegistrationForm from './pages/RegistrationForm/RegistrationForm.jsx'
 
 function App() {
 
   return (
     <>
       {/* Here you are supposed to use components */}
-        <Feed />
+        <RegistrationForm />
     </>
   )
 }
