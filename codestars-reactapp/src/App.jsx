d9175c81042d483ca9e565c6b2d353ed52926add
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-05-31 15:40:22 +0200

    rv: Pulled and resolved conflicts

--- codestars-reactapp/src/App.jsx
+++ codestars-reactapp/src/App.jsx
@@ -3,29 +3,18 @@ import Feed from './pages/Feed.jsx'
 import Navbar from './components/navigation/Navigation';
 import Notifications from './pages/notifications/Notifications';
 import Header from './components/header/Header'
-<<<<<<< HEAD
 import RegistrationForm from './pages/RegistrationForm/RegistrationForm.jsx'
 import Postcard from './components/postcard/Postcard.jsx';
-=======
 // import RegistrationForm from './pages/RegistrationForm/RegistrationForm.jsx'
->>>>>>> 6e4881a0f0270fba321be16ed53591453e785e48
 
 function App() {
 
   return (
     <>
-<<<<<<< HEAD
         {/* Here you are supposed to use components */}
         {/* <Feed /> */}
         {/* <Notifications /> */}
         <RegistrationForm />
-=======
-      {/* Here you are supposed to use components */}
-        {/* <Feed /> */}
-        <Notifications />
-     
-        {/* <RegistrationForm /> */}
->>>>>>> 6e4881a0f0270fba321be16ed53591453e785e48
     </>
   )
 }
