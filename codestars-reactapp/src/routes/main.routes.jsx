3c68ccf6ab0750e6922db53955b296e94fb2208a
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-06-03 10:02:48 +0200

    implemented error 404 page

--- codestars-reactapp/src/routes/main.routes.jsx
+++ codestars-reactapp/src/routes/main.routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from '../App';
 import RegistrationForm from '../pages/RegistrationForm/RegistrationForm'
 import Notifications from '../pages/notifications/Notifications'
+import PageNotFound from '../pages/404/PageNotFound'
 import Feed from '../pages/Feed'
 
 export const router = createBrowserRouter([
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {path: "/", element: <Feed />},
       {path: "register", element: <RegistrationForm />},
       {path: "notifications", element: <Notifications />},
+      {path: "404", element: <PageNotFound />},
+      {path: "search", element: <PageNotFound />},
+      {path: "create-post", element: <PageNotFound />},
+      {path: "messages", element: <PageNotFound />},
       // {path: "test", element: <RegistrationForm />}
     ]},
   ])
