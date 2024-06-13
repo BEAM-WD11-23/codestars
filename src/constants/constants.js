e8155688137fff3822f5a21bf14e812430ced956
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2024-06-13 14:45:20 +0200

    rv: added constants for all 4 databases

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,7 @@
 export const POSTS_URL = 'https://6638ed6c4253a866a24fb5be.mockapi.io/posts';
 export const USERS_URL = 'https://6638ed6c4253a866a24fb5be.mockapi.io/people';
+
+export const MESSAGES_ENDPOINT = `https://666adffb7013419182d149d6.mockapi.io/messages`;
+export const NOTIFICATIONS_ENDPOINT = `https://666adffb7013419182d149d6.mockapi.io/notifications`;
+export const USERS_ENDPOINT = `https://6638ed6c4253a866a24fb5be.mockapi.io/users`;
+export const POSTS_ENDPOINT = `https://6638ed6c4253a866a24fb5be.mockapi.io/posts`;
